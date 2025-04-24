543d3b87f2c50d119135cf557520b37e356f7e7d
Vaughan Logan <vaughan@vaughanlogan.com>
2025-04-24 10:26:39 -0400

    test plant def

---
+++ botany.js
@@ -0,0 +1,24 @@
+var plant = {
+	age: 0,
+	input: {
+		liquid: {
+			water: 1,
+		},
+		solid: {
+			fert: 1,
+		},
+		gas: {
+			co2: 1,
+		},
+		light: {
+			uv: 0,
+			violet: 0.5,
+			blue: 0.5,
+			green: 0.1,
+			yellow: 0.5,
+			orange: 0.5,
+			red: 1,
+			ir: 0,
+		},
+	},
+};
